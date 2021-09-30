9952a4edeeef4649dab1b68ef5181d99aa9dd802
Rommel Quintanilla <rquintanilla@utec.edu.pe>
2021-09-29 19:22:03 -0500

    Adding comments tests

--- tests/validation_test.cpp
+++ tests/validation_test.cpp
@@ -24,6 +24,27 @@ TEST(SimpleTest, basicTest) {
   EXPECT_EQ(tokens, expected);
 }
 
+TEST(CommentTest, hasComments) {
+  std::istrstream str("a = b /* shouldn't be printed */ c = d");
+
+  FlexScanner scanner(str, std::cout);
+  std::vector<Token> tokens;
+
+  while (1) {
+    auto token = scanner.get_token();
+    if (token._atributo == END) break;
+    tokens.push_back(token);
+  }
+
+  std::vector<Token> expected = {Token("a", Categoria::IDENTIFICADOR),
+                                 Token("=", Categoria::ASSIGN),
+                                 Token("b", Categoria::NUMERO),
+                                 Token("c", Categoria::IDENTIFICADOR),
+                                 Token("=", Categoria::ASSIGN),
+                                 Token("d", Categoria::IDENTIFICADOR)};
+  EXPECT_EQ(tokens, expected);
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
